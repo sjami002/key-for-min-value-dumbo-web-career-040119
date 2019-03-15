2e95d15e149e97f4b3b07beeb404185ce5b97381
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-15 22:44:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,7 +11,7 @@ def key_for_min_value(name_hash)
     end
     min_value = array[0]
     array.each do |value|
-      min_value == value if min_value < value
+      min_value == value if value < min_value
     end
     name_hash.each do |key, value|
       if min_value = value
